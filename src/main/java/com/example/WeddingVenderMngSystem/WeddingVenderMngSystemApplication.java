e671cc1d373c361ddb9ae83221e2323fa100ecb6
janithedirisinghe <110591538+janithedirisinghe@users.noreply.github.com>
2025-02-12 22:15:21 +0530

    Initial commit - Spring Boot Project Setup

---
+++ src/main/java/com/example/WeddingVenderMngSystem/WeddingVenderMngSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.WeddingVenderMngSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingVenderMngSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingVenderMngSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WeddingVenderMngSystem/WeddingVenderMngSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WeddingVenderMngSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingVenderMngSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
